ab995625d70ce97dfd3248789176946c0830ce7e
Mustan Pathetic <patheticmustan@gmail.com>
2021-04-05 23:44:15 -0400

    build

--- Assets/Scripts/GameManagerTitle.cs
+++ Assets/Scripts/GameManagerTitle.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// secret sauce things
+using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameManagerTitle : MonoBehaviour {
     public TextMeshProUGUI title;
 
+    // title wiggle!
     private float wiggle;
-
     public int wiggleDegrees = 10;
     public int wiggleSpeed = 1;
+
+    // this will have little bees flying around the screen
+    public Sprite[] assets;
+    public int beeCount = 5;
 
     void Start() {
         wiggle = 0;
